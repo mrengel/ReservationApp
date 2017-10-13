11ae539ca04254d2873184e64ad7b0d9088d5280
mrengel <m.r.engel2010@gmail.com>
2017-10-12 21:20:52 -0400

    File Upload

---
+++ server.js
@@ -0,0 +1,63 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static("style"));
+
+//RESERVATION DATA
+
+var reservations = [{
+  routeName: "yoda",
+  name: "Yoda",
+  role: "Jedi Master",
+  age: 900,
+  forcePoints: 2000
+}];
+
+
+//Routes
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "welcome.html"));
+});
+
+app.get("/view", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "add.html"));
+});
+
+app.get("/all", function(req, res) {
+  res.json(reservations);
+});
+
+// Create New Reservations - takes in JSON input
+app.post("/api/new", function(req, res) {
+  var newreservation = req.body;
+  newreservation.routeName = newreservation.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newreservation);
+
+  reservation.push(newreservation);
+
+  res.json(newreservation);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("our app is running on port: " + PORT);
+});
